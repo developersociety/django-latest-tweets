49adc04942dadc2b17222c50f4a724087034c5aa
Alex Tomkins <tomkins@darkzone.net>
2012-11-16 14:51:47 +0000

    Add files for packaging

---
+++ README.rst
@@ -0,0 +1,5 @@
+====================
+Django Latest Tweets
+====================
+
+A Django app to store the latest tweets from Twitter.
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+# Use latest_tweets.VERSION for version numbers
+version_tuple = __import__('latest_tweets').VERSION
+version = '.'.join([str(v) for v in version_tuple])
+
+setup(
+    name='django-latest-tweets',
+    version=version,
+    description='Latest Tweets for Django',
+    long_description=open('README.rst').read(),
+    url='https://github.com/blancltd/django-latest-tweets',
+    maintainer='Alex Tomkins',
+    maintainer_email='alex@blanc.ltd.uk',
+    platforms=['any'],
+    packages=[
+        'latest_tweets',
+        'latest_tweets.management',
+        'latest_tweets.management.commands',
+        'latest_tweets.templatetags',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    license='BSD-2',
+)
