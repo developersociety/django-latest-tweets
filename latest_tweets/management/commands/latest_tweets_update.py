4fb5500d0fc3c17738ac65bc042dde347903d97b
Alex Tomkins <tomkins@darkzone.net>
2012-11-16 13:04:47 +0000

    Better support for 1.4 timezones

--- latest_tweets/management/commands/latest_tweets_update.py
+++ latest_tweets/management/commands/latest_tweets_update.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.utils.timezone import utc
 from datetime import datetime
 from twitter import Twitter
 from latest_tweets.models import Tweet
@@ -9,10 +10,15 @@ def update_user(user):
     messages = t.statuses.user_timeline(screen_name=user, include_rts=True)
 
     for i in messages:
-        obj, created = Tweet.objects.get_or_create(tweet_id=i['id'], defaults={
-            'user': i['user']['screen_name'],
-            'text': i['text'],
-            'created': datetime.strptime(i['created_at'], '%a %b %d %H:%M:%S +0000 %Y'),
+        tweet_id = i['id']
+        tweet_username = i['user']['screen_name']
+        tweet_text = i['text']
+        tweet_created = datetime.strptime(i['created_at'], '%a %b %d %H:%M:%S +0000 %Y').replace(tzinfo=utc)
+
+        obj, created = Tweet.objects.get_or_create(tweet_id=tweet_id, defaults={
+            'user': tweet_username,
+            'text': tweet_text,
+            'created': tweet_created,
         })
 
 
