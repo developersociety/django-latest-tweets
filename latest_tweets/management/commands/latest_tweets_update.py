a0b5d809dd391212027c6d27dae9c5a7e8da16b3
Alex Tomkins <tomkins@darkzone.net>
2013-02-12 13:15:54 +0000

    HTML entities need to be stripped out

--- latest_tweets/management/commands/latest_tweets_update.py
+++ latest_tweets/management/commands/latest_tweets_update.py
@@ -4,16 +4,21 @@ from django.utils.timezone import utc
 from datetime import datetime
 from twitter import Twitter, OAuth
 from latest_tweets.models import Tweet
+import HTMLParser
 
 
 def update_user(user):
     t = Twitter(auth=OAuth(settings.TWITTER_OAUTH_TOKEN, settings.TWITTER_OAUTH_SECRET, settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET))
     messages = t.statuses.user_timeline(screen_name=user, include_rts=True)
 
+    # Need to escape HTML entities
+    htmlparser = HTMLParser.HTMLParser()
+    unescape = htmlparser.unescape
+
     for i in messages:
         tweet_id = i['id']
         tweet_username = i['user']['screen_name']
-        tweet_text = i['text']
+        tweet_text = unescape(i['text'])
         tweet_created = datetime.strptime(i['created_at'], '%a %b %d %H:%M:%S +0000 %Y').replace(tzinfo=utc)
 
         obj, created = Tweet.objects.get_or_create(tweet_id=tweet_id, defaults={
