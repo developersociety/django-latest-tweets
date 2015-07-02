9cd00d0567e8685a52829e8f4837645fb7ffbcd0
Alex Tomkins <tomkins@darkzone.net>
2015-07-02 15:37:37 +0100

    Show the HTML version as HTML

--- latest_tweets/admin.py
+++ latest_tweets/admin.py
@@ -8,8 +8,18 @@ class TweetAdmin(admin.ModelAdmin):
     list_filter = ('created', 'user')
     date_hierarchy = 'created'
     readonly_fields = (
-        'tweet_id', 'user', 'name', 'text', 'html', 'retweeted_username', 'retweeted_name',
+        'tweet_id', 'user', 'name', 'text', 'tweet_html', 'retweeted_username', 'retweeted_name',
         'retweeted_tweet_id', 'is_reply', 'created')
+    fieldsets = (
+        (None, {
+            'fields': readonly_fields,
+        }),
+    )
 
     def has_add_permission(self, request):
         return False
+
+    def tweet_html(self, obj):
+        return obj.html
+    tweet_html.short_description = 'HTML'
+    tweet_html.allow_tags = True
