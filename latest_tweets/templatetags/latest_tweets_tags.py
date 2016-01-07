8eaa0f2fef26cc90e3aea5dea1253b7980400375
Alex Tomkins <tomkins@darkzone.net>
2016-01-07 13:28:53 +0000

    Add tag support for getting liked tweets

--- latest_tweets/templatetags/latest_tweets_tags.py
+++ latest_tweets/templatetags/latest_tweets_tags.py
@@ -8,11 +8,15 @@ register = template.Library()
 def get_latest_tweets(*args, **kwargs):
     limit = kwargs.pop('limit', None)
     include_replies = kwargs.pop('include_replies', False)
+    liked_by = kwargs.pop('liked_by', None)
     tweets = Tweet.objects.all()
 
     #  By default we exclude replies
     if not include_replies:
         tweets = tweets.exclude(is_reply=True)
+
+    if liked_by:
+        tweets = tweets.filter(like__user=liked_by)
 
     if args:
         tweets = tweets.filter(user__in=args)
