02008407eccf83be739d5b8b2b7f545f1b14a4bf
Alex Tomkins <tomkins@darkzone.net>
2017-11-16 16:54:54 +0000

    Increase display name to 50 characters (was 20)

---
+++ latest_tweets/migrations/0014_display_name_length.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('latest_tweets', '0013_hashtags'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='tweet',
+            name='name',
+            field=models.CharField(max_length=50),
+        ),
+        migrations.AlterField(
+            model_name='tweet',
+            name='retweeted_name',
+            field=models.CharField(blank=True, max_length=50),
+        ),
+    ]
