c69b3d3c0accaf1094f46e429a19937790410b5d
Alex Tomkins <tomkins@darkzone.net>
2020-10-10 00:39:19 +0100

    Update supported versions of Python / Django
    Getting ready for Python 3.5+ Django 1.11+

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
-from codecs import open
+import os
 
 from setuptools import find_packages, setup
 
-with open("README.rst", "r", "utf-8") as f:
-    readme = f.read()
+
+def read(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        return f.read()
 
 
 setup(
     name="django-latest-tweets",
     version="0.4.6",
     description="Latest Tweets for Django",
-    long_description=readme,
+    long_description=read("README.rst"),
+    long_description_content_type="text/x-rst",
     url="https://github.com/developersociety/django-latest-tweets",
     maintainer="The Developer Society",
     maintainer_email="studio@dev.ngo",
     platforms=["any"],
+    python_requires=">=3.5",
     install_requires=[
+        "Django>=1.11",
         "twitter>=1.9.1",
         "requests>=2.0",
     ],
@@ -24,21 +29,18 @@ setup(
     include_package_data=True,
     classifiers=[
         "Environment :: Web Environment",
-        "Framework :: Django",
-        "Framework :: Django :: 1.8",
-        "Framework :: Django :: 1.11",
-        "Framework :: Django :: 2.2",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.2",
     ],
     license="BSD",
 )
